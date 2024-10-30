24883ef80687759dd6c738c1a199e3378c7420cc
Mohamed Fahmy <mohamedfahmy.webdeveloper@gmail.com>
2024-10-30 22:14:28 +0300

    improve change status modal by disabling button while sending request

--- src/modules/orders/orderDetails/ChangeSingleStatusModal.jsx
+++ src/modules/orders/orderDetails/ChangeSingleStatusModal.jsx
@@ -1,24 +1,30 @@
 import { Row, Col, Modal } from "react-bootstrap"
-import React, { useState } from "react"
 import t from "../../../translations.json"
 import { pathOr } from "ramda"
 import { useRouter } from "next/router"
 import { toast } from "react-toastify"
 import axios from "axios"
+import { useState } from "react"
 
 const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
   const { locale } = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+
   const changeOrderStatus = async (statusId) => {
     if (selectedOrder) {
       try {
+        setIsLoading(true)
         await axios.post(`/ChangeOrderStatus?orderId=${selectedOrder?.orderId}&status=${statusId}`)
         toast.success(locale === "en" ? "Order Status Updated Successfully!" : "!تم تغيير حالة المنتج بنجاح")
         setOpenModal(false)
+        setIsLoading(false)
       } catch (error) {
+        setIsLoading(false)
         toast.error(error.response.data.message)
       }
     }
   }
+
   return (
     <Modal show={openModal} onHide={() => setOpenModal(false)} className="mt-5">
       <Modal.Header>
@@ -33,6 +39,7 @@ const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
             <div className="mb-2 text-center">
               <button
                 className={`fs-5 f-b ${selectedOrder?.orderStatus == 1 ? `main-color` : ``}`}
+                disabled={isLoading}
                 onClick={() => changeOrderStatus(1)}
               >
                 {pathOr("", [locale, "Orders", "waiting_for_payment"], t)}
@@ -43,6 +50,7 @@ const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
             <div className="mb-2 text-center">
               <button
                 className={`fs-5 f-b ${selectedOrder?.orderStatus == 2 ? `main-color` : ``}`}
+                disabled={isLoading}
                 onClick={() => changeOrderStatus(2)}
               >
                 {pathOr("", [locale, "Orders", "waiting_for_review"], t)}
@@ -53,6 +61,7 @@ const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
             <div className="mb-2 text-center">
               <button
                 className={`fs-5 f-b ${selectedOrder?.orderStatus == 3 ? `main-color` : ``}`}
+                disabled={isLoading}
                 onClick={() => changeOrderStatus(3)}
               >
                 {pathOr("", [locale, "Orders", "in_progress"], t)}
@@ -63,6 +72,7 @@ const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
             <div className="mb-2 text-center">
               <button
                 className={`fs-5 f-b ${selectedOrder?.orderStatus == 4 ? `main-color` : ``}`}
+                disabled={isLoading}
                 onClick={() => changeOrderStatus(4)}
               >
                 {pathOr("", [locale, "Orders", "ready_for_delivery"], t)}
@@ -73,6 +83,7 @@ const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
             <div className="mb-2 text-center">
               <button
                 className={`fs-5 f-b ${selectedOrder?.orderStatus == 5 ? `main-color` : ``}`}
+                disabled={isLoading}
                 onClick={() => changeOrderStatus(5)}
               >
                 {pathOr("", [locale, "Orders", "delivery_in_progress"], t)}
@@ -83,6 +94,7 @@ const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
             <div className="mb-2 text-center">
               <button
                 className={`fs-5 f-b ${selectedOrder?.orderStatus == 6 ? `main-color` : ``}`}
+                disabled={isLoading}
                 onClick={() => changeOrderStatus(6)}
               >
                 {pathOr("", [locale, "Orders", "delivered"], t)}
@@ -93,6 +105,7 @@ const ChangeStatusModal = ({ openModal, setOpenModal, selectedOrder }) => {
             <div className="mb-2 text-center">
               <button
                 className={`fs-5 f-b ${selectedOrder?.orderStatus == 7 ? `main-color` : ``}`}
+                disabled={isLoading}
                 onClick={() => changeOrderStatus(7)}
               >
                 {pathOr("", [locale, "Orders", "canceled"], t)}
