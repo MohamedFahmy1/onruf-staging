4e59de917acc119eae90e92aa837a048e5435b62
Mohamed Fahmy <mohamedfahmy.webdeveloper@gmail.com>
2026-08-18 15:42:42 +0300

    Revert framework upgrade and retain loader removal

---
+++ src/middleware.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server"
+
+export default function middleware(req) {
+  let url = req.url
+  let domainName =
+    process.env.NODE_ENV === "development" ? "http://localhost:3040/" : process.env.NEXT_PUBLIC_BUSINESS_URL
+  if (
+    url.includes("businessAccountId") &&
+    url.includes("ProviderId") &&
+    url.includes("Token") &&
+    url.includes("UserId")
+  ) {
+    let parsedUrl = new URL(url, domainName)
+    let params = new URLSearchParams(parsedUrl.search)
+    let businessAccountId = params.get("businessAccountId")
+    let providerId = params.get("ProviderId")
+    let token = params.get("Token")
+    let userId = params.get("UserId")
+    let cookies = []
+    if (businessAccountId) {
+      cookies.push(`businessAccountId=${businessAccountId}; Path=/;`)
+    }
+    if (providerId) {
+      cookies.push(`ProviderId=${providerId}; Path=/;`)
+    }
+    if (token) {
+      cookies.push(`Token=${token}; Path=/;`)
+    }
+    if (userId) {
+      cookies.push(`UserId=${userId}; Path=/;`)
+    }
+    const response = NextResponse.redirect(domainName)
+    for (let cookie of cookies) {
+      response.headers.append("Set-Cookie", cookie)
+    }
+    return response
+  }
+  return NextResponse.next()
+}
